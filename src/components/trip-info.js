5b7429ce1c27a1cd5129fde173e28f7c2430a554
Uporold <crubick1337@gmail.com>
2020-04-15 18:56:05 +0300

    Исправления

--- src/components/trip-info.js
+++ src/components/trip-info.js
@@ -1,11 +1,12 @@
 import {createElement} from "../utils";
 
 const createTripInfoTemplate = (trail, days) => {
+  const daysInterval = `${days.length ? `${days[0].month} ${days[0].day}&nbsp;&nbsp;&mdash;&nbsp;${days[0].month !== days[days.length - 1].month ? days[days.length - 1].month : ``} ${days[days.length - 1].day}` : ``}`;
   return (
     `<section class="trip-main__trip-info  trip-info">
         <div class="trip-info__main">
           <h1 class="trip-info__title">${trail}</h1>
-          <p class="trip-info__dates">${days[0].month} ${days[0].day}&nbsp;&nbsp;&mdash;&nbsp;${days[0].month !== days[days.length - 1].month ? days[days.length - 1].month : ``} ${days[days.length - 1].day}</p>
+           <p class="trip-info__dates">${daysInterval}</p>
         </div>
      </section>`
   );
--- src/utils.js
+++ src/utils.js
@@ -67,22 +67,9 @@ export const getRandomIntegerNumber = (min, max) => {
 };
 
 export const getNoRepeatingDates = (events) => {
-  let arr = [];
-  for (let k = 0; k < events.length; k++) {
-    let day = events[k].startDate.getDate();
-    let monthId = events[k].startDate.getMonth();
-    let month = months[monthId];
-    arr.push({day, month});
-  }
-
-  return arr.reduce((acc, current) => {
-    const x = acc.find((item) => item.day === current.day);
-    if (!x) {
-      return acc.concat([current]);
-    } else {
-      return acc;
-    }
-  }, []);
+  const set = new Set();
+  events.forEach((evt) => set.add(JSON.stringify({day: evt.startDate.getDate(), month: months[evt.startDate.getMonth()]})));
+  return Array.from(set).map((evt) => JSON.parse(evt));
 };
 
 export const createElement = (template, groupFlag = false) => {
